604446351521a1cb64f9e509dfdc5dc33a0b342f
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-13 16:59:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,9 @@
 def my_each(array)
-  if block_given? && kind_of(array) == true
   i = 0
-  new_array = []
+  
      while i < array.length 
      yield (array(i))
-     new_array = new_array << i
+     i
     end
-  return new_array
-else
-  return "No block given!"
-end
+  return array
 end
