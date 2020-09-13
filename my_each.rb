28b47846983eb6eb92fd96400752c93a96cb7701
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-13 16:34:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(input)
+  if block_given?
+  i = 0 
+    if input == strl
+     while i < strl.length 
+     yield (strl(i))
+     i 
+    end
+    elsif input == int
+  string
+else
+  return "No block given!"
+end
 end
