ccca68b4be1c79a898f0a291961ffeef1e143f58
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-13 16:47:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,7 @@ def my_each(array)
   i = 0
   new_array = []
      while i < array.length 
-     yield (array(i)*n)
+     yield (array(i))
      new_array = new_array << i
     end
   return new_array
